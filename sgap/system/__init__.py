10ad6edd980a56b6145716b6f3c937308d498e1d
Shawn Presser <shawnpresser@gmail.com>
2021-08-18 05:37:11 -0500

    WIP

---
+++ sgap/system/__init__.py
@@ -0,0 +1,38 @@
+import subprocess
+from pathlib import Path
+from typing import Any, Dict, Optional
+import shlex
+import sys
+
+from .. import util
+
+
+def run(cmd: str, *args: str, stdin=None, verbose=True, strip=True, encoding="utf-8", error_output=sys.stderr, shell=False):
+    if shell:
+        if args:
+            cmd += ' ' + shlex.join(args)
+    else:
+        cmd = [cmd] + args
+    del args
+    p = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE if stdin is not None else None,
+        encoding=encoding,
+        shell=shell,
+    )
+    stdout, stderr = p.communicate(input=stdin)
+    stdout = util.convert_string(stdout)
+    stderr = util.convert_string(stderr)
+    if p.returncode == 0:
+        if stderr:
+            if verbose:
+                print("`%s` printed to stderr:" % cmd, file=error_output)
+            print(stderr.rstrip(), file=error_output)
+        if strip:
+            stdout = stdout.rstrip("\r\n")
+        return stdout
+    if verbose:
+        print("`%s` returned %s" % (cmd, p.returncode), file=error_output)
+    if stderr:
+        print(stderr.rstrip(), file=error_output)
+    raise subprocess.CalledProcessError("Command '%s' returned non-zero exit status %d" % (cmd, p.returncode))
---
+++ sgap/util.py
@@ -0,0 +1,18 @@
+def to_bytes(str_input) -> bytes:
+    # Encode to UTF-8 to get binary data.
+    if isinstance(str_input, bytes):
+        return str_input
+    return str_input.encode("utf-8")
+
+def to_string(bytes_input) -> str:
+    if isinstance(bytes_input, str):
+        return bytes_input
+    return bytes_input.decode("utf-8")
+
+def convert_string(bytes_input) -> str:
+    try:
+        return to_string(bytes_input.decode("utf-8"))
+    except AttributeError:  # 'str' object has no attribute 'decode'.
+        return str(bytes_input)
+    except UnicodeError:
+        return str(bytes_input)
